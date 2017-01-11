aaf1fcf1a012263b811cc709935625dafc3b72dd
Cynthia (Qingwei) Li <cynngah@gmail.com>
2017-01-10 20:12:48 -0500

    can get stuff from form in controller now

--- pickachamp/app/controllers/winrates_controller.rb
+++ pickachamp/app/controllers/winrates_controller.rb
@@ -2,6 +2,37 @@
 
 class WinratesController < ApplicationController
     def get_winrates
+        p params
+        p params[:myc]
+        p params[:c1]
+        p params[:c2]
+        p params[:c3]
+        p params[:c4]
+        p params[:c5]
         @winrates = Winrate.all
     end
+
+    # def new
+    #     @champs = Winrate.new
+    # end
+    #
+    # def index
+    #     @winrates = Winrate.all
+    #
+    # def new
+    #   @message = Message.new
+    # end
+    # def create
+    #   @message = Message.new(message_params)
+    #   if @message.save
+    #     redirect_to '/messages'
+    #   else
+    #     render 'new'
+    #   end
+    # end
+    #
+    # private
+    #   def message_params
+    #     params.require(:message).permit(:content)
+    #   end
 end
